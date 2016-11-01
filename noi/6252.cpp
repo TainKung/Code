acd752c937b52d902a3e2255a4e86c52208bd662
TainKung <tainkung@foxmail.com>
2016-11-01 21:25:17 +0800

    Sync.

---
+++ noi/6252.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstring>
+// #include <iostream>
+using namespace std;
+
+const int kMax=30;
+bool a[kMax][kMax];
+char sa[kMax], sb[kMax];
+int la, lb;
+
+int main() {
+    scanf("%s", sa);
+    scanf("%s", sb);
+    la = strlen(sa);
+    lb = strlen(sb);
+
+    memset(a, 0, sizeof(a));
+    a[0][0] = 1;
+    for (int i=1; i<=la; i++) for (int j=1; j<=lb; j++) {
+        switch (sa[i-1]) {
+            case '?':
+                a[i][j] = a[i-1][j-1];
+                break;
+            case '*':
+                for (int k=j; k>=0 && !a[i][j]; k--) 
+                    a[i][j] |= a[i-1][k];
+                break;
+            default:
+                if (sa[i-1] == sb[j-1]) a[i][j] = a[i-1][j-1];
+        }
+    }
+
+    printf(a[la][lb]? "matched\n" : "not matched\n");
+    return 0;
+}
